3b07303ebf20ba405c2301717a4f4de8f1b4567f
Hadi <mhadimollataheri@gmail.com>
2024-07-07 12:06:25 +0330

    Initialize and Install dependencies

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className="m-2 p-1 text-purple-400 shadow-md">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
