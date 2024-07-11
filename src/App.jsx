3b2b851c08bd08fe67ac110717c28cf94f2c52c4
Hadi <mhadimollataheri@gmail.com>
2024-07-11 10:07:12 +0330

    Install dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import "./App.css";
+import { Button } from '@chakra-ui/react';
 
 function App() {
   return (
     <>
-      <p className="m-2 p-1 text-purple-400 shadow-md">
+      <p className='m-2 bg-green-500 p-1 font-semibold text-purple-800 shadow-xl'>
         Click on the Vite and React logos to learn more
       </p>
+      <Button colorScheme={'green'} ml={5}>
+        Button
+      </Button>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+
+// Changing dark mode bg color to black
+// const styles = {
+//   global: (props) => ({
+//     'body': {
+//       backgroundColor: 
+//     }
+//   })
+// }
+
+// const config = {
+//   initialColorMode: 'dark',
+//   useSystemColorMode: false,
+// };
+// const theme = extendTheme({ config });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider >
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
-)
+);
