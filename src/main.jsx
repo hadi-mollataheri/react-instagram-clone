4b5892c7b38ab3b58c14e91a448dfde2dfff4770
Hadi <mhadimollataheri@gmail.com>
2024-07-11 18:28:04 +0330

    Initital the app with dark theme -Change the default bg and color dark theme

--- src/main.jsx
+++ src/main.jsx
@@ -3,25 +3,28 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { mode } from '@chakra-ui/theme-tools';
 
 // Changing dark mode bg color to black
-// const styles = {
-//   global: (props) => ({
-//     'body': {
-//       backgroundColor: 
-//     }
-//   })
-// }
+const styles = {
+  global: (props) => ({
+    body: {
+      bg: mode('white', 'black')(props),
+      color: mode('black', 'white')(props),
+    },
+  }),
+};
 
-// const config = {
-//   initialColorMode: 'dark',
-//   useSystemColorMode: false,
-// };
-// const theme = extendTheme({ config });
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+};
+// extendTheme will merge it argument into theme object
+const theme = extendTheme({ config, styles });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider >
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
