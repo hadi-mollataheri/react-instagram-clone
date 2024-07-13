6d679fbc5935092eb70ab1d5d4f640a499f478c9
Hadi <mhadimollataheri@gmail.com>
2024-07-13 11:10:46 +0330

    Install react-router-dom and react-icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Button } from '@chakra-ui/react';
+import { FaHome } from 'react-icons/fa';
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
       <p className='m-2 bg-green-500 p-1 font-semibold text-purple-800 shadow-xl'>
         Click on the Vite and React logos to learn more
       </p>
+      <FaHome className='text-blue-500' />
       <Button colorScheme={'green'} ml={5}>
         Button
       </Button>
