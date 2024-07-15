d0633a9793921d1dda7804f6952259bfd2b29321
Hadi <mhadimollataheri@gmail.com>
2024-07-15 11:28:27 +0330

    Organiz the pictures put pictures in assets folder and import them into the pictures.js and store them as an object and default export it

---
+++ src/assets/pictures.js
@@ -0,0 +1,23 @@
+import auth from './auth.png';
+import google from './google.png';
+import img1 from './img1.png';
+import img2 from './img2.png';
+import img3 from './img3.png';
+import img4 from './img4.png';
+import logo from './logo.png';
+import playstore from './playstore.png';
+import profilePicture from './profilePicture.jpg';
+
+const PICTURES = {
+  auth,
+  google,
+  img1,
+  img2,
+  img3,
+  img4,
+  logo,
+  playstore,
+  profilePicture,
+};
+
+export default PICTURES;
--- src/pages/Authentication.jsx
+++ src/pages/Authentication.jsx
@@ -1,10 +1,12 @@
+import PICTURES from '../assets/pictures';
 
 const Authentication = () => {
   return (
     <div>
       Authentication
+      <img src={PICTURES.auth} alt='instagram sample page' />
     </div>
-  )
-}
+  );
+};
 
-export default Authentication
+export default Authentication;
