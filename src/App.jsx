0c287d5cb0fac6752078b173a15926e4cab7c8b9
Hadi <mhadimollataheri@gmail.com>
2024-07-15 10:35:09 +0330

    Create Home and Auth components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { Button } from '@chakra-ui/react';
-import { FaHome } from 'react-icons/fa';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import Authentication from './pages/Authentication.jsx';
 
 function App() {
   return (
-    <>
-      <p className='m-2 bg-green-500 p-1 font-semibold text-purple-800 shadow-xl'>
-        Click on the Vite and React logos to learn more
-      </p>
-      <FaHome className='text-blue-500' />
-      <Button colorScheme={'green'} ml={5}>
-        Button
-      </Button>
-    </>
+    <Router basename='/react-instagram-clone'>
+      <>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='auth' element={<Authentication />} />
+        </Routes>
+      </>
+    </Router>
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+  return (
+    <div className='font-bold'>
+      Hi there! Im home
+    </div>
+  );
+};
+
+export default Home;
