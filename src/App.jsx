9cc97b6c10e33a3cf6a73e6957f0fb0b954c94f8
Hadi <mhadimollataheri@gmail.com>
2024-07-27 15:56:56 +0330

    Remove Authentiation component repalce it with LogIn component and change styles for .logIn-container in LogIn.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
-import Authentication from './pages/Authentication.jsx';
+import LogIn from './pages/LogIn.jsx';
 import EmailSignUp from './pages/EmailSignUp.jsx';
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
       <div className='text-white'>
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='auth' element={<Authentication />} />
+          <Route path='login' element={<LogIn />} />
           <Route path='emailSignUp' element={<EmailSignUp />} />
         </Routes>
       </div>
