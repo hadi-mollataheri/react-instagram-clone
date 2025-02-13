3ef8d69763e10f3430fc9109787d49bf5fbf5676
Hadi-MT <mhadimollataheri@gmail.com>
2025-02-13 10:57:51 +0330

    Create basic structure of howe page including the menu and suggestions sections for the big screens(Creating grid)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,46 +13,108 @@ import { Menu, MenuButton, MenuList, MenuItem, Button } from '@chakra-ui/react';
 
 const Home = () => {
   return (
-    <div>
-      {/* Navigation menu for mobile screens */}
-      <nav className='flex items-center justify-between px-5'>
+    <div className='mt-5 sm:grid sm:grid-cols-3 sm:grid-rows-1'>
+      <nav className='sm flex items-center justify-between px-5 sm:h-lvh sm:w-52 sm:flex-col sm:items-start sm:justify-start sm:border-r-2 sm:border-r-gray-500'>
         <div id='logo-container'>
           <img src={PICTURES.logoColor} alt='instagram' className='w-32' />
         </div>
-        <Menu>
-          <MenuButton as={Button} size='sm'>
-            <List size={22} />
-          </MenuButton>
-          <MenuList>
-            <MenuItem value='home'>
-              <HouseLine size={24} weight='fill' className='mr-1.5' />
-              Home
-            </MenuItem>
-            <MenuItem value='search'>
-              <MagnifyingGlass size={24} className='mr-1.5' />
-              Search
-            </MenuItem>
-            <MenuItem value='notification'>
-              <Heart size={24} className='mr-1.5' />
-              Notification
-            </MenuItem>
-            <MenuItem value='create'>
-              <PlusSquare size={24} className='mr-1.5' />
-              Create
-            </MenuItem>
-            <MenuItem value='profile' paddingLeft='2'>
-              <UserCircle size={28} weight='fill' className='mr-1.5' /> Profile
-            </MenuItem>
-            <MenuItem>
-              <SignOut size={24} className='mr-1.5' />
-              LogOut
-            </MenuItem>
-          </MenuList>
-        </Menu>
+        <div id='mobile-menu' className='sm:hidden'>
+          <Menu>
+            <MenuButton as={Button} size='sm'>
+              <List size={22} />
+            </MenuButton>
+            <MenuList>
+              <MenuItem value='home'>
+                <HouseLine size={24} weight='fill' className='mr-1.5' />
+                Home
+              </MenuItem>
+              <MenuItem value='search'>
+                <MagnifyingGlass size={24} className='mr-1.5' />
+                Search
+              </MenuItem>
+              <MenuItem value='notification'>
+                <Heart size={24} className='mr-1.5' />
+                Notification
+              </MenuItem>
+              <MenuItem value='create'>
+                <PlusSquare size={24} className='mr-1.5' />
+                Create
+              </MenuItem>
+              <MenuItem value='profile' paddingLeft='2'>
+                <UserCircle size={28} weight='fill' className='mr-1.5' />{' '}
+                Profile
+              </MenuItem>
+              <MenuItem>
+                <SignOut size={24} className='mr-1.5' />
+                LogOut
+              </MenuItem>
+            </MenuList>
+          </Menu>
+        </div>
+        <div
+          id='screen-menu'
+          className='hidden sm:flex sm:h-screen sm:flex-col sm:justify-between sm:py-10'
+        >
+          <ul className='leading-[60px]'>
+            <li>
+              <a href='#'>
+                <HouseLine
+                  size={32}
+                  weight='fill'
+                  className='mr-4 inline-block'
+                />
+                Home
+              </a>
+            </li>
+            <li>
+              <a href='#'>
+                <MagnifyingGlass size={32} className='mr-4 inline-block' />
+                Search
+              </a>
+            </li>
+            <li>
+              <a href='#'>
+                <Heart size={32} className='mr-4 inline-block' />
+                Notification
+              </a>
+            </li>
+            <li>
+              <a href='#'>
+                <PlusSquare size={32} className='mr-4 inline-block' />
+                Create
+              </a>
+            </li>
+            <li>
+              <a href='#'>
+                <UserCircle
+                  size={32}
+                  weight='fill'
+                  className='mr-4 inline-block'
+                />
+                Profile
+              </a>
+            </li>
+          </ul>
+
+          <a href='#'>
+            <SignOut size={32} className='mr-4 inline-block' />
+            LogOut
+          </a>
+        </div>
       </nav>
-      <main>
-        <h2 className='mt-32 text-center opacity-50'>No posts to show!</h2>
+
+      <main className=''>
+        <h2 className='mt-32 text-center opacity-50 sm:mt-0 sm:pt-32'>
+          No posts to show!
+        </h2>
       </main>
+
+      <section
+        id='suggestions'
+        className='mr-20 mt-[29.5px] hidden justify-self-end px-5 sm:block'
+      >
+        Suggestions
+      </section>
     </div>
   );
 };
