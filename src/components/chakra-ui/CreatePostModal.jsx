5d68f83514bb7ffab64781c4c27cffdd3621e5a1
Hadi-MT <mhadimollataheri@gmail.com>
2025-02-18 12:36:52 +0330

    Create users from empty posting by checking the length of the text and images and alerting them

--- src/components/chakra-ui/CreatePostModal.jsx
+++ src/components/chakra-ui/CreatePostModal.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/prop-types */
+import { useState } from 'react';
 import { useUserStoreSelectors } from '../../stores/user-store';
 import {
   Modal,
@@ -17,11 +18,16 @@ import {
 } from '@chakra-ui/react';
 
 const CreatePostModal = ({ isOpen, onClose }) => {
+  const [emptyNessCheck, setEmptyNessCheck] = useState(null);
+
   const postText = useUserStoreSelectors.use.postText();
   const updatePostText = useUserStoreSelectors.use.updatePostText();
   const postImages = useUserStoreSelectors.use.postImages();
   const updatePostImages = useUserStoreSelectors.use.updatePostImages();
 
+  const handlePostTextChange = ({ target }) => {
+    updatePostText(target.value); // update the postText state with the new value from the input field
+  };
   // It allows users that preview their selected files and also updating the postImages state
   const handleImageChange = (event) => {
     const files = event.target.files; //contains the FileList object including the selected files by the user
@@ -34,13 +40,20 @@ const CreatePostModal = ({ isOpen, onClose }) => {
 
   // Function to handle form submission
   const handleSubmit = () => {
-    //TODO:Update Your Home Component AND Placeholder for post submission logic
-    console.log('Post Text:', postText);
-    console.log('Selected Images:', postImages);
-    // Clear the fields
-    updatePostText('');
-    updatePostImages([]);
-    onClose();
+    // Check if both postText and postImages are not empty before submitting the form
+    if (postText.trim().length <= 0 || postImages.length <= 0) {
+      setEmptyNessCheck(true);
+      return;
+    } else {
+      //TODO:Update Your Home Component AND Placeholder for post submission logic
+      console.log('Post Text:', postText);
+      console.log('Selected Images:', postImages);
+      // Clear the fields
+      updatePostText('');
+      updatePostImages([]);
+      setEmptyNessCheck(false);
+      onClose();
+    }
   };
 
   return (
@@ -54,9 +67,10 @@ const CreatePostModal = ({ isOpen, onClose }) => {
           <Textarea
             placeholder='Write something interesting...'
             value={postText}
-            onChange={(e) => updatePostText(e.target.value)}
+            onChange={handlePostTextChange}
             mb={4}
           />
+
           {/* File input for image selection */}
 
           <Input
@@ -82,6 +96,12 @@ const CreatePostModal = ({ isOpen, onClose }) => {
           )}
         </ModalBody>
         <ModalFooter>
+          {emptyNessCheck === true && (
+            <p className='text-sm text-red-300'>
+              <span className='text-2xl text-red-600'>*</span>Please fill out
+              the post text and select at least one image before posting!
+            </p>
+          )}
           <Button variant='outline' mr={3} onClick={onClose}>
             Cancel
           </Button>
