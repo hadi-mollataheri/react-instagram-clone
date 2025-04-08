a0172c475cb2d6fadec43332e49fe426fb2f2f20
Hadi-MT <mhadimollataheri@gmail.com>
2025-04-08 11:36:37 +0330

    Adjust the forward and backward buttons -Move them inside the images -Remove them when there is only one image

--- src/components/Home/Post.jsx
+++ src/components/Home/Post.jsx
@@ -17,22 +17,32 @@ function Post({ post }) {
   };
 
   return (
-    <div className='post-container m-14 border'>
-      <div className='images-container flex'>
-        <button className='backward-button' onClick={handleBackward}>
-          &#8592;
-        </button>
+    <div className='post-container mx-auto my-12 flex max-w-96 flex-col'>
+      <div className='images-container relative flex items-center justify-center'>
         <img
           src={post.images[imageIndex]}
           alt={post.content}
-          className='m-2 rounded object-cover'
+          className='mx-2 h-48 w-[90%] rounded object-cover'
         />
-        <button className='forward-button' onClick={handleForward}>
-          &#8594;
-        </button>
+        {post.images.length >= 2 && (
+          <>
+            <button
+              className='backward-button absolute left-5 top-1/2 h-8 w-8 -translate-y-1/2 transform rounded-full bg-slate-800 pb-1 text-lg font-bold leading-none opacity-50 hover:opacity-100 active:bg-slate-700 active:delay-[60ms]'
+              onClick={handleBackward}
+            >
+              &#8592;
+            </button>
+            <button
+              className='forward-button absolute right-5 top-1/2 h-8 w-8 -translate-y-1/2 transform rounded-full bg-slate-800 pb-1 text-lg font-bold leading-none opacity-50 hover:opacity-100 active:bg-slate-700 active:delay-[60ms]'
+              onClick={handleForward}
+            >
+              &#8594;
+            </button>
+          </>
+        )}
       </div>
       <div className='content-container'>
-        <p>{post.content}</p>
+        <p className='pl-[5.3%] pt-4 text-sm'>{post.content}</p>
       </div>
     </div>
   );
