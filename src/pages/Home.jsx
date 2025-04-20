6aab639e51c6b5548fe5438e0398c8cac3aabed3
Hadi-MT <mhadimollataheri@gmail.com>
2025-04-20 16:14:51 +0330

    Add copywrite section to main page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -116,114 +116,121 @@ const Home = () => {
   }, [updateUserPosts, setIsLoading]);
 
   return (
-    <div className='mt-5 sm:grid sm:grid-cols-3 sm:grid-rows-1'>
-      <nav className='sm flex items-center justify-between px-5 sm:w-52 sm:flex-col sm:items-start sm:justify-start sm:border-r-2 sm:border-r-gray-500'>
-        <div id='logo-container'>
-          <img src={PICTURES.logoColor} alt='instagram' className='w-32' />
-        </div>
-        <div id='mobile-menu' className='sm:hidden'>
-          <Menu>
-            <MenuButton as={Button} size='sm'>
-              <List size={22} />
-            </MenuButton>
-            <MenuList>
-              <MenuItem value='home'>
-                <HouseLine size={24} weight='fill' className='mr-1.5' />
-                Home
-              </MenuItem>
-              <MenuItem value='search'>
-                <MagnifyingGlass size={24} className='mr-1.5' />
-                Search
-              </MenuItem>
-              <MenuItem value='notification'>
-                <Heart size={24} className='mr-1.5' />
-                Notification
-              </MenuItem>
-              <MenuItem value='create' onClick={onOpen}>
-                <PlusSquare size={24} className='mr-1.5' />
-                Create
-              </MenuItem>
-              <MenuItem value='profile' paddingLeft='2'>
-                <UserCircle size={28} weight='fill' className='mr-1.5' />{' '}
-                Profile
-              </MenuItem>
-              <MenuItem>
-                <SignOut size={24} className='mr-1.5' />
-                LogOut
-              </MenuItem>
-            </MenuList>
-          </Menu>
-        </div>
-        <div
-          id='screen-menu'
-          className='hidden sm:flex sm:h-screen sm:flex-col sm:items-start sm:justify-evenly sm:py-10'
+    <div className='parent-container'>
+      <div className='nav-main-suggestion-container mt-5 sm:grid sm:grid-cols-3 sm:grid-rows-1'>
+        <nav className='sm flex items-center justify-between px-5 sm:w-52 sm:flex-col sm:items-start sm:justify-start sm:border-r-2 sm:border-r-gray-500'>
+          <div id='logo-container'>
+            <img src={PICTURES.logoColor} alt='instagram' className='w-32' />
+          </div>
+          <div id='mobile-menu' className='sm:hidden'>
+            <Menu>
+              <MenuButton as={Button} size='sm'>
+                <List size={22} />
+              </MenuButton>
+              <MenuList>
+                <MenuItem value='home'>
+                  <HouseLine size={24} weight='fill' className='mr-1.5' />
+                  Home
+                </MenuItem>
+                <MenuItem value='search'>
+                  <MagnifyingGlass size={24} className='mr-1.5' />
+                  Search
+                </MenuItem>
+                <MenuItem value='notification'>
+                  <Heart size={24} className='mr-1.5' />
+                  Notification
+                </MenuItem>
+                <MenuItem value='create' onClick={onOpen}>
+                  <PlusSquare size={24} className='mr-1.5' />
+                  Create
+                </MenuItem>
+                <MenuItem value='profile' paddingLeft='2'>
+                  <UserCircle size={28} weight='fill' className='mr-1.5' />{' '}
+                  Profile
+                </MenuItem>
+                <MenuItem>
+                  <SignOut size={24} className='mr-1.5' />
+                  LogOut
+                </MenuItem>
+              </MenuList>
+            </Menu>
+          </div>
+          <div
+            id='screen-menu'
+            className='hidden sm:flex sm:h-screen sm:flex-col sm:items-start sm:justify-evenly sm:py-10'
+          >
+            <ul className='leading-[60px]'>
+              <li>
+                <a href='#'>
+                  <HouseLine
+                    size={32}
+                    weight='fill'
+                    className='mr-4 inline-block'
+                  />
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href='#'>
+                  <MagnifyingGlass size={32} className='mr-4 inline-block' />
+                  Search
+                </a>
+              </li>
+              <li>
+                <a href='#'>
+                  <Heart size={32} className='mr-4 inline-block' />
+                  Notification
+                </a>
+              </li>
+              <li>
+                <button onClick={onOpen}>
+                  <PlusSquare size={32} className='mr-4 inline-block' />
+                  Create
+                </button>
+              </li>
+              <li>
+                <a href='#'>
+                  <UserCircle
+                    size={32}
+                    weight='fill'
+                    className='mr-4 inline-block'
+                  />
+                  Profile
+                </a>
+              </li>
+            </ul>
+
+            <button href='#'>
+              <SignOut size={32} className='mr-4 inline-block' />
+              LogOut
+            </button>
+          </div>
+        </nav>
+
+        <main className=''>
+          {isLoading ? (
+            <img src={PICTURES.loading} alt='loading' />
+          ) : userPosts && userPosts.length > 0 ? (
+            userPosts.map((post) => <Post key={post.id} post={post} />)
+          ) : (
+            <h2 className='mt-32 text-center opacity-50 sm:mt-0 sm:pt-32'>
+              No posts to show!
+            </h2>
+          )}
+        </main>
+
+        <section
+          id='suggestions'
+          className='mr-20 mt-[29.5px] hidden justify-self-end px-5 sm:block'
         >
-          <ul className='leading-[60px]'>
-            <li>
-              <a href='#'>
-                <HouseLine
-                  size={32}
-                  weight='fill'
-                  className='mr-4 inline-block'
-                />
-                Home
-              </a>
-            </li>
-            <li>
-              <a href='#'>
-                <MagnifyingGlass size={32} className='mr-4 inline-block' />
-                Search
-              </a>
-            </li>
-            <li>
-              <a href='#'>
-                <Heart size={32} className='mr-4 inline-block' />
-                Notification
-              </a>
-            </li>
-            <li>
-              <button onClick={onOpen}>
-                <PlusSquare size={32} className='mr-4 inline-block' />
-                Create
-              </button>
-            </li>
-            <li>
-              <a href='#'>
-                <UserCircle
-                  size={32}
-                  weight='fill'
-                  className='mr-4 inline-block'
-                />
-                Profile
-              </a>
-            </li>
-          </ul>
-
-          <button href='#'>
-            <SignOut size={32} className='mr-4 inline-block' />
-            LogOut
-          </button>
-        </div>
-      </nav>
-
-      <main className=''>
-        {isLoading ? (
-          <img src={PICTURES.loading} alt='loading' />
-        ) : userPosts && userPosts.length > 0 ? (
-          userPosts.map((post) => <Post key={post.id} post={post} />)
-        ) : (
-          <h2 className='mt-32 text-center opacity-50 sm:mt-0 sm:pt-32'>
-            No posts to show!
-          </h2>
-        )}
-      </main>
-
-      <section
-        id='suggestions'
-        className='mr-20 mt-[29.5px] hidden justify-self-end px-5 sm:block'
-      >
-        Suggestions
-      </section>
+          Suggestions
+        </section>
+      </div>
+      <footer className='w-full self-center'>
+        <p className='mb-2 w-full text-center text-sm'>
+          &copy; 2025 built by Hadi mTaheri
+        </p>
+      </footer>
 
       <CreatePostModal isOpen={isOpen} onClose={onClose} />
     </div>
